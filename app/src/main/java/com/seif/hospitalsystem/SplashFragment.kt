f6316b5dc4c962a303de70dc18bba39dd31695fa
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-02-23 18:53:40 +0200

    splash and prototype screen added

---
+++ app/src/main/java/com/seif/hospitalsystem/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.seif.hospitalsystem
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_prototypeFragment)
+        },1600L)
+    }
+}
