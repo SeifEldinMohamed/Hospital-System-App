ca6a569100b73c02bead12844db9c9e786431bb7
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-03-05 00:10:17 +0200

    calls screen added

--- app/src/main/java/com/seif/hospitalsystem/ui/fragments/CasesFragment.kt
+++ app/src/main/java/com/seif/hospitalsystem/ui/fragments/CasesFragment.kt
@@ -1,14 +1,23 @@
 package com.seif.hospitalsystem.ui.fragments
 
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.seif.hospitalsystem.R
 import com.seif.hospitalsystem.data.models.Case
 import com.seif.hospitalsystem.data.models.Status
 import com.seif.hospitalsystem.databinding.FragmentCasesBinding
 import com.seif.hospitalsystem.ui.CasesAdapter
+import java.util.*
 
 
 class CasesFragment : Fragment() {
@@ -23,10 +32,53 @@ class CasesFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         casesAdapter.addCases(createDummyCases())
         binding.rvCases.adapter = casesAdapter
+
+        val calender = Calendar.getInstance()
+//        val dataPicker: DatePickerDialog.OnDateSetListener =
+//            DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+//                calender.set(Calendar.YEAR, year)
+//                calender.set(Calendar.MONTH, month)
+//                calender.set(Calendar.DAY_OF_MONTH, day)
+//                updateLabel(calender)
+//            }
+        binding.calendarEditText.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(
+                requireContext(),
+                R.style.BottomSheetDialogTheme
+            )
+            val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
+                R.layout.buttomsheet_layout,
+                view.findViewById<ConstraintLayout>(R.id.buttom_sheet)
+            )
+            bottomSheetView.findViewById<CalendarView>(R.id.calendarView)
+                ?.setOnDateChangeListener { view, year, month, dayOfMonth ->
+                    calender.set(Calendar.YEAR, year)
+                    calender.set(Calendar.MONTH, month)
+                    calender.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    updateLabel(calender)
+                    bottomSheetDialog.dismiss()
+                }
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun updateLabel(calender: Calendar) {
+        val sdf = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val format = "dd-MM-yyyy"
+            SimpleDateFormat(format, Locale.UK)
+        }
+        else { // check it again //////////////////////////////////////////////////////
+            val format = "dd-MM-yyyy"
+            SimpleDateFormat(format, Locale.UK)
+        }
+        binding.calendarEditText.setText(sdf.format(calender.time))
     }
     private fun createDummyCases():List<Case>{
         return listOf<Case>(
