799ecb9e2a95500a7d76d7ad9763885a52fba198
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-04-02 21:39:27 +0200

    hr profile screen added

--- app/src/main/java/com/seif/hospitalsystem/HrProfileFragment.kt
+++ app/src/main/java/com/seif/hospitalsystem/HrProfileFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.seif.hospitalsystem.databinding.FragmentHrProfileBinding
 
 
 class HrProfileFragment : Fragment() {
-
+lateinit var binding: FragmentHrProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hr_profile, container, false)
+        binding = FragmentHrProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
